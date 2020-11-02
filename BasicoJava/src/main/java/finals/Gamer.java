2e4ff066b16488528203032b754397595729a54b
lanasreis <lanareissdd@gmail.com>
2020-11-02 20:10:43 -0300

    Conteudo finals

---
+++ BasicoJava/src/main/java/finals/Gamer.java
@@ -0,0 +1,12 @@
+package finals;
+
+public class Gamer {
+    public String keyboard() {
+        return "Keyboard Gamer!";
+    }
+
+    public final String mouse() {
+        return "Mouse Gamer!";
+    }
+
+}
---
+++ BasicoJava/src/main/java/finals/Programa.java
@@ -0,0 +1,19 @@
+package finals;
+
+public class Programa {
+    public static void main(String[] args) {
+        final HardcoreGamer hardcoreGamer = new HardcoreGamer();
+
+        final Gamer gamer = new Gamer();
+
+        final CasualGamer casualGamer = new CasualGamer();
+
+        final var game = "PUBG";
+
+        //game = "WoW";
+
+        System.out.println(casualGamer.play(game));
+
+
+    }
+}
