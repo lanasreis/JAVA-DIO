843f8602b372b0b56e42349533afc487c4093da8
lanasreis <lanareissdd@gmail.com>
2020-11-08 12:01:25 -0300

    Aula LSP(Liskov Substitution Principle) definicao e exemplos

---
+++ SOLID/src/main/java/LSP/Definicao.java
@@ -0,0 +1,8 @@
+package LSP;
+
+public class Definicao {
+    /*
+        Classes derivadas devem ser substituíveis por suas classes base
+        Se para cada objeto o1 do tipo S há um objeto o2 do tipo T, de forma que, para todos os programas P, o comportamento de P é inalterado quando o1 é substituído por o2, então S é um subtipo de T
+    */
+}
---
+++ SOLID/src/main/java/LSP/problema/Main.java
@@ -0,0 +1,12 @@
+package LSP.problema;
+
+public class Main {
+    public static void main(String[] args){
+        Retangulo retangulo = new Quadrado();
+
+        retangulo.setAltura(10);
+        retangulo.setLargura(5);
+
+        System.out.println("Area: " + retangulo.getArea());
+    }
+}
