57b5423ba0d0e3fcb74316f3a30b43a3ff2e75bc
lanasreis <lanareissdd@gmail.com>
2020-11-14 15:32:46 -0300

    Spring boot profiles

---
+++ Profiles/src/main/java/Definicao.java
@@ -0,0 +1,16 @@
+public class Definicao {
+    /*
+        Importância dos profiles:
+        -Múltiplos ambientes:
+                *LocalDevelopment - Development - Staging - Production
+           -Bancos de dados para cada ambiente?
+           -Execução de testes unitários em ambiente local?
+           -Supite de teste completasem ambiente de teste
+           -Simulação do ambiente real em staging
+           -Deploy simplificado em produção
+
+       Spring boot Profiles
+       -Configurações próprias para cada tido de ambiente
+       - Ambientes com sua configuração: dev, production
+    */
+}
